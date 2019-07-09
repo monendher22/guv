55fc0e15642053edaae6243c30ec352e50953aa8
monendher22 <52447170+monendher22@users.noreply.github.com>
2019-07-09 14:14:47 +0530

    Create repeated.py

---
+++ repeated.py
@@ -0,0 +1,12 @@
+s == int(input())
+arr = int().split()
+arr2 =[]
+for  i in range(s):
+  for j in range(1+s,n);
+    if arr[i]=arr[j] and arr[i] not in arr2:
+      arr2.append(arr[i])
+arr2.sort()
+if len(arr2) == 0:
+  print('unique')
+else:
+  print(' '.join(arr2))
